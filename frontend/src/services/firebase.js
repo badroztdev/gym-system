7e1ee022728fcb1533df6f71220673c8da05cc6d
badroztdev <badrozt618@gmail.com>
2026-08-26 15:32:31 +0100

    firebase console  2

--- frontend/src/services/firebase.js
+++ frontend/src/services/firebase.js
@@ -2,13 +2,15 @@
 import { initializeApp } from "firebase/app";
 import { getMessaging, getToken, onMessage } from "firebase/messaging";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey:            "AIzaSyAe799OC4vtFnwem00kkJDOnw_dlg34xhs",
-  authDomain:        "gym-pro-fe5fb.firebaseapp.com",
-  projectId:         "gym-pro-fe5fb",
-  storageBucket:     "gym-pro-fe5fb.firebasestorage.app",
+  apiKey: "AIzaSyAe799OC4vtFnwem00kkJDOnw_dlg34xhs",
+  authDomain: "gym-pro-fe5fb.firebaseapp.com",
+  projectId: "gym-pro-fe5fb",
+  storageBucket: "gym-pro-fe5fb.firebasestorage.app",
   messagingSenderId: "112881979310",
-  appId:             "1:112881979310:web:16888301414641fafc423f",
+  appId: "1:112881979310:web:16888301414641fafc423f",
+  measurementId: "G-G44TQPLBHX"
 };
 
 const VAPID_KEY = "BKOqhlNjzYnTAfze9y4jbXl6xpc4QzeSZK3aRR3cH6pTSrc2rS_GEkSrM0RXf_USj42ODjMnY2H5Y_kQ6QJc0VQ";
